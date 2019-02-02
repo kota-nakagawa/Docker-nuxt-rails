aaa9e4bfb2efae16e7ab6f059db745c2786f93d2
nyanko_kota <public@nyanko-kota.com>
2019-02-02 16:14:35 +0900

    Update development.rb

--- docker/rails/app/config/environments/development.rb
+++ docker/rails/app/config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.web_console.whitelisted_ips = '172.20.0.1'
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -58,4 +58,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-end
+end 
